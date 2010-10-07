b6285e21cb9c9023b3d6d41827122a0d0715f2e9
Алексей Шельф <despawn@gmail.com>
2010-10-08 00:56:46 +0400

    connections: Remove whitespace

--- fallen/connections.py
+++ fallen/connections.py
@@ -19,52 +19,52 @@ def connection_property(name):
 
 
 class Connections(GObject.GObject):
-    
+
     __gsignals__ = {
         'connected': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, []),
         'disconnected': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, [])
     }
-    
+
     instance = None
     up = False
     connections = dict()
 
     path = None
-    
+
     def __new__(cls, *args, **kwargs):
         if cls.instance is None:
             cls.instance = GObject.GObject.__new__(cls, *args, **kwargs)
         return cls.instance
-        
+
     def __getitem__(self, handle):
         if not self.up:
             return None
         if not handle in self.connections:
             self.__connect(handle)
         return self.connections[handle]
-        
+
     def __connect(self, handle):
         c = Connection()
         c.connect(self.path)
         self.connections[handle] = c
-        
+
     def __disconnect(self, handle):
         c = self.connections[handle]
         c.disconnect()
         self.connections[handle] = None        
-       
+
     def bring_up(self):
         for handle in self.connections:
             self.__connect(handle)
         self.up = True
         self.emit('connected')
-            
+
     def bring_down(self):
         for handle in self.connections:
             self.__disconnect(handle)
         self.up = False
         self.emit('disconnected')
-        
+
 
 class Connection(XMMS):
 
@@ -76,7 +76,7 @@ class Connection(XMMS):
         self.__out = 0
         self.__in = GObject.io_add_watch(self.__fd, GObject.IO_IN,
                                          self.__handle_in)
-                                         
+
     def disconnect(self):
         # FIXME: Doesn't work due to a refcount bug in xmms2 bindings
         if self.__in: GObject.source_remove(self.__in)
@@ -85,7 +85,7 @@ class Connection(XMMS):
         del self.__out, self.__in, self.__has_out, self.__fd
 
     # -------------------------------------------------------------------------
-                                                                                 
+
     def __need_out(self, need):
         if self.want_ioout() and not self.__has_out:
             self.__has_out = True
