c6357d7b5b332ea7ec90a68befc22fc508f42095
Алексей Шельф <despawn@gmail.com>
2010-10-08 00:53:54 +0400

    connections: Port to g-i GObject

--- fallen/connections.py
+++ fallen/connections.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from xmmsclient import XMMS
-import gobject
+from gi.repository import GObject
 
 
 def result_handler(proc):
@@ -18,11 +18,11 @@ def connection_property(name):
     return property(getter)
 
 
-class Connections(gobject.GObject):
+class Connections(GObject.GObject):
     
     __gsignals__ = {
-        'connected': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, []),
-        'disconnected': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, [])
+        'connected': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, []),
+        'disconnected': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, [])
     }
     
     instance = None
@@ -33,7 +33,7 @@ class Connections(gobject.GObject):
     
     def __new__(cls, *args, **kwargs):
         if cls.instance is None:
-            cls.instance = gobject.GObject.__new__(cls, *args, **kwargs)
+            cls.instance = GObject.GObject.__new__(cls, *args, **kwargs)
         return cls.instance
         
     def __getitem__(self, handle):
@@ -74,33 +74,32 @@ class Connection(XMMS):
         self.__fd = self.get_fd()
         self.__has_out = False
         self.__out = 0
-        self.__in = gobject.io_add_watch(
-                                self.__fd, gobject.IO_IN, self.__handle_in)
+        self.__in = GObject.io_add_watch(self.__fd, GObject.IO_IN,
+                                         self.__handle_in)
                                          
     def disconnect(self):
         # FIXME: Doesn't work due to a refcount bug in xmms2 bindings
-        if self.__in: gobject.source_remove(self.__in)
-        if self.__out: gobject.source_remove(self.__out)
+        if self.__in: GObject.source_remove(self.__in)
+        if self.__out: GObject.source_remove(self.__out)
         self.set_need_out_fun(None)
         del self.__out, self.__in, self.__has_out, self.__fd
 
-    # ------------------------------------------------------------------
+    # -------------------------------------------------------------------------
                                                                                  
     def __need_out(self, need):
         if self.want_ioout() and not self.__has_out:
             self.__has_out = True
-            self.__out = gobject.io_add_watch(
-                                     self.__fd, gobject.IO_OUT, self.__handle_out)
+            self.__out = GObject.io_add_watch(self.__fd, GObject.IO_OUT,
+                                              self.__handle_out)
 
     def __handle_in(self, source, condition):
-        if condition == gobject.IO_IN:
+        if condition == GObject.IO_IN:
             self.ioin()
         return True
 
     def __handle_out(self, source, condition):
-        if condition == gobject.IO_OUT:
+        if condition == GObject.IO_OUT:
             self.ioout()
         self.__has_out = self.want_ioout()
         return self.__has_out
 
-    
